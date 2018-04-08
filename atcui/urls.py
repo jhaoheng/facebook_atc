90a71972bbaf20337b5d825821f9295d57829247
jhaoheng <jhaoheng@gmail.com>
2018-04-08 09:11:38 +0000

    init

---
+++ atcui/urls.py
@@ -0,0 +1,30 @@
+"""atcui URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic.base import RedirectView
+from django.conf.urls import include
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    # Django ATC API
+    url(r'^api/v1/', include('atc_api.urls')),
+    # Django ATC Demo UI
+    url(r'^atc_demo_ui/', include('atc_demo_ui.urls')),
+    # Django ATC profile storage
+    url(r'^api/v1/profiles/', include('atc_profile_storage.urls')),
+    url(r'^$', RedirectView.as_view(url='/atc_demo_ui/', permanent=False)),
+]
